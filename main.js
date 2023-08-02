c744594e9d3e274dff59a9975e8d4903a15184fd
Jalal-12345 <130084355+Jalal-12345@users.noreply.github.com>
2023-08-02 13:41:25 +0300

    main.js

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ let modal_content = document.querySelector(".modal-content");
 btnSerch.addEventListener("click", ()=>{
 
     modal.style.display = "flex"
-    modal_content.classList.add("active");
 })
 
 let clos = document.querySelector(".close");
 clos .addEventListener("click" , ()=>{
-    modal_content.classList.remove("active");
    modal.style.display = "none";
 })
 
